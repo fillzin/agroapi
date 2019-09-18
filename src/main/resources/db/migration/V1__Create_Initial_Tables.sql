899f481ddaac98dccdbc19c1ca10f5fedd964fd5
Fillipe <fillipem08@gmail.com>
2019-09-18 02:02:33 -0300

    reduzindo tamanho das colunas por compatibilidade

--- src/main/resources/db/migration/V1__Create_Initial_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Initial_Tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `oauth_approvals` (
-  `userId` varchar(256) DEFAULT NULL,
-  `clientId` varchar(256) DEFAULT NULL,
-  `scope` varchar(256) DEFAULT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `clientId` varchar(255) DEFAULT NULL,
+  `scope` varchar(255) DEFAULT NULL,
   `status` varchar(10) DEFAULT NULL,
   `expiresAt` timestamp NULL DEFAULT NULL,
   `lastModifiedAt` timestamp NULL DEFAULT NULL
@@ -10,25 +10,25 @@ CREATE TABLE `oauth_approvals` (
 CREATE TABLE `oauth_client_details` (
   `client_id` varchar(255) NOT NULL,
   `client_secret` varchar(255) NOT NULL,
-  `web_server_redirect_uri` varchar(2048) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(255) DEFAULT NULL,
   `scope` varchar(255) DEFAULT NULL,
   `access_token_validity` int(11) DEFAULT NULL,
   `refresh_token_validity` int(11) DEFAULT NULL,
-  `resource_ids` varchar(1024) DEFAULT NULL,
-  `authorized_grant_types` varchar(1024) DEFAULT NULL,
-  `authorities` varchar(1024) DEFAULT NULL,
-  `additional_information` varchar(4096) DEFAULT NULL,
+  `resource_ids` varchar(255) DEFAULT NULL,
+  `authorized_grant_types` varchar(255) DEFAULT NULL,
+  `authorities` varchar(255) DEFAULT NULL,
+  `additional_information` varchar(255) DEFAULT NULL,
   `autoapprove` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`client_id`)
 );
 
 CREATE TABLE `oauth_code` (
-  `code` varchar(256) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
   `authentication` mediumblob
 );
 
 CREATE TABLE `oauth_refresh_token` (
-  `token_id` varchar(256) DEFAULT NULL,
+  `token_id` varchar(255) DEFAULT NULL,
   `token` mediumblob,
   `authentication` mediumblob
 );
@@ -58,22 +58,22 @@ CREATE TABLE `permission_role` (
 );
 
 CREATE TABLE `oauth_access_token` (
-  `token_id` varchar(256) DEFAULT NULL,
+  `token_id` varchar(255) DEFAULT NULL,
   `token` mediumblob,
-  `authentication_id` varchar(256) NOT NULL,
-  `user_name` varchar(256) DEFAULT NULL,
-  `client_id` varchar(256) DEFAULT NULL,
+  `authentication_id` varchar(255) NOT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `client_id` varchar(255) DEFAULT NULL,
   `authentication` mediumblob,
-  `refresh_token` varchar(256) DEFAULT NULL,
+  `refresh_token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`authentication_id`)
 );
 
 CREATE TABLE `oauth_client_token` (
-  `token_id` varchar(256) DEFAULT NULL,
+  `token_id` varchar(255) DEFAULT NULL,
   `token` mediumblob,
-  `authentication_id` varchar(256) NOT NULL,
-  `user_name` varchar(256) DEFAULT NULL,
-  `client_id` varchar(256) DEFAULT NULL,
+  `authentication_id` varchar(255) NOT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `client_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`authentication_id`)
 );
 
